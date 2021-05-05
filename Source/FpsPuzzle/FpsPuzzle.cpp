9c3d4865a85d9fe6509e892fee2084b284beb22d
규 서문 <illsi@naver.com>
2021-05-05 18:00:33 +0900

    Fps Puzzle

---
+++ Source/FpsPuzzle/FpsPuzzle.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FpsPuzzle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FpsPuzzle, "FpsPuzzle" );
---
+++ Source/FpsPuzzle/FpsPuzzleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FpsPuzzleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSPUZZLE_API AFpsPuzzleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
